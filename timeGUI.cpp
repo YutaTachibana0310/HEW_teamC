e4605e863d8ab3b567fb7c6c4fb89b3a72eeef69
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-03-07 18:56:30 +0900

    タイムGUIに小数点を実装。サイズを調整

--- timeGUI.cpp
+++ timeGUI.cpp
@@ -12,18 +12,23 @@
 #define TIMEGUI_NUMTEX_NAME				"data/TEXTURE/SCORE/number002.png"
 #define TIMEGUI_NUMTEX_SIZE_X			(40.0f)
 #define TIMEGUI_NUMTEX_SIZE_Y			(50.0f)
-#define TIMEGUI_NUMTEX_BASEPOS_1P		(D3DXVECTOR3(SCREEN_WIDTH - TIMEGUI_NUMTEX_SIZE_X, 110.0f, 0.0f))
-#define TIMEGUI_NUMTEX_BASEPOS_2P		(D3DXVECTOR3(WINDOW_WIDTH - TIMEGUI_NUMTEX_SIZE_X, 110.0f, 0.0f))
+#define TIMEGUI_NUMTEX_BASEPOS_1P		(D3DXVECTOR3(SCREEN_WIDTH - TIMEGUI_NUMTEX_SIZE_X, 90.0f, 0.0f))
+#define TIMEGUI_NUMTEX_BASEPOS_2P		(D3DXVECTOR3(WINDOW_WIDTH - TIMEGUI_NUMTEX_SIZE_X, 90.0f, 0.0f))
 #define TIMEGUI_NUMTEX_OFFSET			(-25.0f)
 #define TIMEGUI_NUMTEX_DIV_X			(10)
 #define TIMEGUI_NUMTEX_DECIMAL_DIGIT	(2)
 #define TIMEGUI_NUMTEX_OFFSET_INT		(-10.0f)
 
 #define TIMEGUI_TEXTTEX_NAME			"data/TEXTURE/SCORE/timeText.png"
-#define TIMEGUI_TEXTTEX_SIZE_X			(240.0f)
+#define TIMEGUI_TEXTTEX_SIZE_X			(180.0f)
 #define TIMEGUI_TEXTTEX_SIZE_Y			(TIMEGUI_TEXTTEX_SIZE_X / 3.0f)
-#define TIMEGUI_TEXTTEX_BASEPOS_1P		(D3DXVECTOR3(SCREEN_WIDTH - 400.0f, 50.0f, 0.0f))
-#define TIMEGUI_TEXTTEX_BASEPOS_2P		(D3DXVECTOR3(WINDOW_WIDTH - 400.0f, 50.0f, 0.0f))
+#define TIMEGUI_TEXTTEX_BASEPOS_1P		(D3DXVECTOR3(SCREEN_WIDTH - 150.0f, 30.0f, 0.0f))
+#define TIMEGUI_TEXTTEX_BASEPOS_2P		(D3DXVECTOR3(WINDOW_WIDTH - 150.0f, 30.0f, 0.0f))
+
+#define TIMEGUI_DECIMALPOINT_NAME		"data/TEXTURE/SCORE/decimalPoint.png"
+#define TIMEGUI_DECIMALPOINT_SIZE_X		(TIMEGUI_NUMTEX_SIZE_X)
+#define TIMEGUI_DECIMALPOINT_SIZE_Y		(TIMEGUI_NUMTEX_SIZE_Y)
+#define TIMEGUI_DECIMALPOINT_OFFSET		(-17.0f)
 
 /**************************************
 構造体定義
@@ -32,7 +37,7 @@
 /**************************************
 グローバル変数
 ***************************************/
-static LPDIRECT3DTEXTURE9 numTex, textTex;
+static LPDIRECT3DTEXTURE9 numTex, textTex, decimalTex;
 static VERTEX_2D vtxWk[NUM_VERTEX];
 
 /**************************************
@@ -41,6 +46,7 @@ static VERTEX_2D vtxWk[NUM_VERTEX];
 void MakeVertexTimeGUI(void);
 void SetTimeGUI(int num, int digit, bool is1P);
 void SetTimeText(bool is2P);
+void SetTimeDecimalPoint(bool is1P);
 
 /**************************************
 初期化処理
@@ -50,6 +56,7 @@ void InitTimeGUI(int num)
 	LPDIRECT3DDEVICE9 pDevice = GetDevice();
 	numTex = CreateTextureFromFile((LPSTR)TIMEGUI_NUMTEX_NAME, pDevice);
 	textTex = CreateTextureFromFile((LPSTR)TIMEGUI_TEXTTEX_NAME, pDevice);
+	decimalTex = CreateTextureFromFile((LPSTR)TIMEGUI_DECIMALPOINT_NAME, pDevice);
 	MakeVertexTimeGUI();
 }
 
@@ -60,6 +67,7 @@ void UninitTimeGUI(int num)
 {
 	SAFE_RELEASE(numTex);
 	SAFE_RELEASE(textTex);
+	SAFE_RELEASE(decimalTex);
 }
 
 /**************************************
@@ -89,14 +97,26 @@ void DrawTimeGUI(float time1P, float time2P)
 	pDevice->SetTexture(0, numTex);
 	for (int i = 0; i < TARGETPLAYER_MAX; i++)
 	{
-		int time = i == 0 ? time1P * 100 : time2P * 100;
+		int time = (int)(i == 0 ? time1P * powf(10, TIMEGUI_NUMTEX_DECIMAL_DIGIT) : time2P * powf(10, TIMEGUI_NUMTEX_DECIMAL_DIGIT));
 		int digitMax = time == 0 ? 1 : (int)log10f((float)time) + 1;
+
+		if (digitMax <= TIMEGUI_NUMTEX_DECIMAL_DIGIT)
+			digitMax = TIMEGUI_NUMTEX_DECIMAL_DIGIT + 1;
+
 		for (int digit = 0; digit < digitMax; digit++, time /= 10)
 		{
 			int num = time % 10;
 			SetTimeGUI(num, digit, i == 0);
 			pDevice->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, NUM_POLYGON, vtxWk, sizeof(VERTEX_2D));
 		}
+	}
+
+	//小数点を描画
+	pDevice->SetTexture(0, decimalTex);
+	for (int i = 0; i < TARGETPLAYER_MAX; i++)
+	{
+		SetTimeDecimalPoint(i == 0);
+		pDevice->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, NUM_POLYGON, vtxWk, sizeof(VERTEX_2D));
 	}
 }
 
@@ -156,4 +176,26 @@ void SetTimeGUI(int num, int digit, bool is1P)
 	vtxWk[1].tex = D3DXVECTOR2((num + 1) * sizeX, 0.0f);
 	vtxWk[2].tex = D3DXVECTOR2(num * sizeX, 1.0f);
 	vtxWk[3].tex = D3DXVECTOR2((num + 1) * sizeX, 1.0f);
+}
+
+/**************************************
+小数点設定処理
+***************************************/
+void SetTimeDecimalPoint(bool is1P)
+{
+	//頂点座標設定
+	D3DXVECTOR3 pos = is1P ? TIMEGUI_NUMTEX_BASEPOS_1P : TIMEGUI_NUMTEX_BASEPOS_2P;
+	pos.x += (TIMEGUI_NUMTEX_DECIMAL_DIGIT - 1) * TIMEGUI_NUMTEX_OFFSET;
+	pos.x += TIMEGUI_DECIMALPOINT_OFFSET;
+
+	vtxWk[0].vtx = pos + D3DXVECTOR3(-TIMEGUI_DECIMALPOINT_SIZE_X / 2.0f, -TIMEGUI_DECIMALPOINT_SIZE_Y / 2.0f, 0.0f);
+	vtxWk[1].vtx = pos + D3DXVECTOR3(TIMEGUI_DECIMALPOINT_SIZE_X / 2.0f, -TIMEGUI_DECIMALPOINT_SIZE_Y / 2.0f, 0.0f);
+	vtxWk[2].vtx = pos + D3DXVECTOR3(-TIMEGUI_DECIMALPOINT_SIZE_X / 2.0f, TIMEGUI_DECIMALPOINT_SIZE_Y / 2.0f, 0.0f);
+	vtxWk[3].vtx = pos + D3DXVECTOR3(TIMEGUI_DECIMALPOINT_SIZE_X / 2.0f, TIMEGUI_DECIMALPOINT_SIZE_Y / 2.0f, 0.0f);
+
+	//UV座標設定
+	vtxWk[0].tex = D3DXVECTOR2(0.0f, 0.0f);
+	vtxWk[1].tex = D3DXVECTOR2(1.0f, 0.0f);
+	vtxWk[2].tex = D3DXVECTOR2(0.0f, 1.0f);
+	vtxWk[3].tex = D3DXVECTOR2(1.0f, 1.0f);
 }
