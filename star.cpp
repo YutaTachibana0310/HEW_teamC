afc9d647301d66fe70a287c941afadb692b8b0c7
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-03-09 12:11:21 +0900

    スターのリリース処理が重複して呼ばれていたのを修正

--- star.cpp
+++ star.cpp
@@ -718,6 +718,9 @@ void UpdateStar(void)
 
 	for (int cntStar = 0; cntStar < MAX_STAR; cntStar++)
 	{
+		if (!star[cntStar].use)
+			continue;
+
 		STAR *wkStar = &star[cntStar];
 		for (int cntBlock = 0; cntBlock < MAX_BLOCK; cntBlock++)
 		{
